18db44b53ca2aebce84c7c094116c013aedc41bb
李大兵 <bzero1982@gmail.com>
2017-02-27 17:38:03 +0800

    添加one vs all
    多分类的 one vs all 预测 是把所有训练的h 都跑一次 然后选取对应h(i)概率最大值

--- ng/homework/machine-learning-ex3/ex3/oneVsAll.m
+++ ng/homework/machine-learning-ex3/ex3/oneVsAll.m
@@ -1,4 +1,4 @@
-function [all_theta] = oneVsAll(X, y, num_labels, lambda)
+  function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %ONEVSALL trains multiple logistic regression classifiers and returns all
 %the classifiers in a matrix all_theta, where the i-th row of all_theta 
 %corresponds to the classifier for label i
@@ -49,11 +49,19 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+    % Set Initial theta
+     initial_theta = zeros(n + 1, 1);
 
+    % Set options for fminunc
+     options = optimset('GradObj', 'on', 'MaxIter', 50);
+     
 
-
-
-
+% Run fmincg to obtain the optimal theta
+%     % This function will return theta and the cost 
+for c = 1:num_labels
+   all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+                 initial_theta, options);
+end
 
 
 
